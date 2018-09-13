4e75295b7cc8ec2af53dd47bff92e689335f2936
musa jamshed <Musa@musas-air.lan>
2018-09-13 16:30:10 -0400

    updated team members

--- team.c
+++ team.c
@@ -3,10 +3,10 @@
 /* Please be sure to fill in this struct right away! */
 struct team_t team =
 {
-	"", /* first member name   */
-	"", /* first member email  */
-	"", /* second member name  */
-	"", /* second member email */
+	"Musa Jamshed", /* first member name   */
+	"msj219@lehigh.edu", /* first member email  */
+	"Thor Gilman", /* second member name  */
+	"tgg219@lehigh.edu", /* second member email */
 	"", /* third member name   */
 	"", /* third member email  */
 };
