f8020c9c72100a481c1e07bd48700595601114df
musa jamshed <Musa@musas-air.lan>
2018-09-17 18:23:45 -0400

    Made progress on part1b, still getting a segfault ho

--- part1b.c
+++ part1b.c
@@ -1,12 +1,51 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include <dlfcn.h>
 #include"support.h"
+
 
 /* load_and_invoke() - load the given .so and execute the specified function */
 void load_and_invoke(char *libname, char *funcname)
 {
+	// https://linux.die.net/man/3/dlsym
+	// https://stackoverflow.com/questions/1354537/dlsym-dlopen-with-runtime-arguments
+	// https://gist.github.com/yellowbyte/ec470d75ba7c14ebefed271c6fe58e9e
+
 	/* TODO: complete this function */
+	void *handle;
+	void (*func)(); // declare void function with no params
+	char *error;
+
+	// this all came from the linux man page
+	handle = dlopen(libname,RTLD_LAZY);
+	if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+
+	// this all came from the linux man page
+	dlerror();    /* Clear any existing error */
+
+	// this all came from the linux man page
+	*(void **) (&func) = dlsym(handle, funcname);
+
+	// this all came from the linux man page
+	if ((error = dlerror()) != NULL)  {
+        fprintf(stderr, "%s\n", error);
+        exit(EXIT_FAILURE);
+	}
+
+	// this all came from the linux man page
+	dlclose(handle);
+
+	// run function
+	(*func)();
+
+	fprintf(stdout,"we made it fam🙏🙏🙏🙏🙏🙏🙏🙏\n");
+
+	exit(EXIT_SUCCESS);
+
 }
 
 /* help() - Print a help message. */
@@ -27,18 +66,24 @@ int main(int argc, char **argv)
 	/* run a student name check */
 	check_team(argv[0]);
 
+
+	//	MUSA commented this command line options part out
+	//  We can worry about implementing the args when it works
+	//
 	/* parse the command-line options. For this program, we only support */
 	/* the parameterless 'h' option, for getting help on program usage.  */
-	while((opt = getopt(argc, argv, "h")) != -1)
+/* 	while((opt = getopt(argc, argv, "h")) != -1)
 	{
 		switch(opt)
 		{
 		case 'h':	help(argv[0]); 	break;
 		}
-	}
+	} */
 
 	/* call load_and_invoke() to run the given function of the given library */
-	load_and_invoke(NULL, NULL);
+	// this has my path, couldn't figure out how to fix it without that
+	// NOTE: I'm using the void test() function, which I declared in libpart1.c
+	load_and_invoke("/home/msj219/cse303/cse303.p1/obj64/libpart1.so", "test");
 
 	exit(0);
 }
