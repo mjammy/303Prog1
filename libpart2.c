44283438b241e7e50509037ab9a4db74a0182da7
musa jamshed <Musa@musas-MacBook-Air.local>
2018-09-20 18:38:48 -0400

    part 2 phase 2 DONE

--- libpart2.c
+++ libpart2.c
@@ -7,6 +7,7 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<stdarg.h>
+#include<stdlib.h>
 
 /* We aren't providing much code here.  You'll need to implement quite a bit
  * for your library. */
@@ -72,53 +73,64 @@ int fscanf(FILE *stream, const char *format, ...) {
 
 int open(const char *pathname, int flags, ...) {
 	// // Store the phase number
+
+	static int (*origopen)(const char *pathname, int flags,...) = NULL;
+	origopen = (int (*)(const char*, int, ...))dlsym(RTLD_NEXT, "open");
+
+	mode_t openMode;
+
 	if (isdigit(*pathname)) {
 		phaseNum = pathname;
 	}
 
-	static int (*origopen)(const char *pathname, int flags) = NULL;
-	origopen = (int (*)(const char*, int))dlsym(RTLD_NEXT, "open");
+	if (strcmp(phaseNum, "0")==0)  {
+		openMode = 0600;
+		return origopen(pathname, flags, openMode);
+
+	}
+
+	if (strcmp(phaseNum, "1")==0)  {
+		openMode = 00;
+		return origopen(pathname, flags, openMode);
+
+	}
+
 	return origopen(pathname, flags);
 }
 
 
-FILE *fopen(const char *filename, const char *mode) {
+/* FILE *fopen(const char *filename, const char *mode) {
 
 	static FILE* (*origfopen)(const char *filename, const char *mode) = NULL;
 	origfopen = (FILE* (*)(const char*, const char *))dlsym(RTLD_NEXT, "fopen");
 	origfopen(filename, mode);
 
-}
+} */
 
 
 ssize_t write(int fd, const void *buf, size_t count){
 	static ssize_t (*origwrite)(int fd, const void *buf, size_t count) = NULL;
 	origwrite = (ssize_t (*)(int, const void*, size_t))dlsym(RTLD_NEXT, "write");
 
-
 	if (strcmp(phaseNum, "2")==0)  {
 
-
-		if (fd == 3) { // alice
-			int* currentBal = malloc(sizeof(int));
-			read(fd, currentBal, sizeof(int));
-			//printf("%d\n", *currentBal);
+		if (fd == 4) { // alice
+			int currBal = atoi((char*)buf);
+			currBal = currBal + 200;
+			char temp_buffer[5];
+			snprintf(temp_buffer, 5, "%d", currBal);
+			buf = (const void*)temp_buffer;
 		}
-		else if (fd == 4) { // bob
-			int* currentBal = malloc(sizeof(int));
-			read(fd, currentBal, sizeof(int));
-			//printf("%d\n", *currentBal);
+		
+		else if (fd == 3) { // bob
+			int currBal = atoi((char*)buf);
+			currBal = currBal - 200;
+			char temp_buffer[5];
+			snprintf(temp_buffer, 5, "%d", currBal);
+			buf = (const void*)temp_buffer;
 		}
-
-
-		//printf("%d\n", fd);
 	}
-
-
-
 	return origwrite(fd, buf, count);
-
-
 }
 
 
